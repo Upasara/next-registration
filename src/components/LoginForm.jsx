f183df0c8c462034e937d355749cc74e5b616240
Upasara <mihirangaupasara2000@gmail.com>
2024-06-22 12:56:42 +0530

    changes to the interface

--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -5,6 +5,9 @@ import Link from 'next/link';
 import { useState } from 'react';
 import { useRouter } from 'next/navigation';
 
+import { FaFacebookF, FaGoogle, FaRegEnvelope } from 'react-icons/fa';
+import { MdLockOutline } from 'react-icons/md';
+
 export default function LoginForm() {
 	const [email, setEmail] = useState();
 	const [password, setPassword] = useState();
@@ -33,34 +36,75 @@ export default function LoginForm() {
 	};
 
 	return (
-		<div className='grid place-items-center h-screen'>
-			<div className='shadow-lg p-5 rounded-lg border-solid border-2 border-fuchsia-400 '>
-				<h1 className='text-2xl font-bold my-4'>Login Form</h1>
+		<div className='flex flex-col items-center justify-center min-h-screen bg-gray-100'>
+			<div className='shadow-2xl px-6 py-8 rounded-xl  bg-white '>
+				<div className='text-left font-bold text-sm text-gray-700'>
+					<span className='text-fuchsia-500'>XYZ </span>Company
+				</div>
 
-				<form onSubmit={handleSubmit} className='flex flex-col gap-6'>
-					<input
-						onChange={(e) => setEmail(e.target.value)}
-						type='email'
-						placeholder='email'
-					/>
-					<input
-						onChange={(e) => setPassword(e.target.value)}
-						type='password'
-						placeholder='password'
-					/>
-					<button className='bg-fuchsia-600 text-white font-bold cursor-pointer rounded-lg px-6 py-3 '>
-						LOGIN
-					</button>
+				<h1 className='text-2xl font-bold mb-8 mt-10 text-center font-mono'>
+					Login Form
+				</h1>
+
+				<form onSubmit={handleSubmit} className='flex flex-col gap-6 '>
+					<div className='bg-gray-100 w-70 p-2 flex items-center rounded-md pl-4 shadow-sm'>
+						<FaRegEnvelope className='text-gray-400 m-2' />
+						<input
+							onChange={(e) => setEmail(e.target.value)}
+							type='email'
+							placeholder='E-mail'
+							className='bg-gray-100 outline-none flex-1'
+						/>
+					</div>
+					<div className='bg-gray-100 w-70 p-2 flex items-center rounded-md pl4 shadow-sm'>
+						<MdLockOutline className='text-gray-400 m-2' />
+						<input
+							onChange={(e) => setPassword(e.target.value)}
+							type='password'
+							placeholder='Password'
+							className='bg-gray-100 outline-none flex-1'
+						/>
+					</div>
+					<div className='mx-auto'>
+						<button className='text-md bg-fuchsia-600 text-white font-bold cursor-pointer rounded-full px-10 py-2  border-2 border-white hover:bg-white hover:text-fuchsia-600 hover:border-2 hover:border-solid hover:border-fuchsia-600'>
+							LOGIN
+						</button>
+					</div>
+					<Link
+						className='mb-5 text-right text-xs text-gray-600'
+						href={'/register'}
+					>
+						Don&apos;t have an Account &nbsp;
+						<span className='underline'>Register</span>
+					</Link>
+				</form>
+				<div className='flex justify-center items-center py-6'>
+					<div className='bg-gray-500 h-0.5 w-full'></div>
+					<p className='text-bold text-gray-500'>&nbsp;&nbsp;OR&nbsp;&nbsp;</p>
+					<div className='h-0.5 w-full bg-gray-500'></div>
+				</div>
+				<div className='flex justify-center my-2 gap-5'>
+					<a
+						href='#'
+						className='border-2 border-gray-300 rounded-full p-3 mx-1'
+					>
+						<FaFacebookF className='text-md text-blue-500' />
+					</a>
+					<a
+						href='#'
+						className='border-2 border-gray-300 rounded-full p-3 mx-1'
+					>
+						<FaGoogle className='text-md text-red-500' />
+					</a>
+				</div>
+
+				<div className='flex flex-col justify-center'>
 					{error && (
 						<div className='bg-red-500 text-white w-fit text-sm py-1 px-3 rounded-sm mt-2'>
 							{error}
 						</div>
 					)}
-					<Link className='mt-3 text-right text-sm' href={'/register'}>
-						Don&apos;t have an Account
-						<span className='underline'>Register</span>
-					</Link>
-				</form>
+				</div>
 			</div>
 		</div>
 	);
--- src/components/RegisterForm.jsx
+++ src/components/RegisterForm.jsx
@@ -3,6 +3,8 @@
 import Link from 'next/link';
 import { useState } from 'react';
 import { useRouter } from 'next/navigation';
+import { FaRegEnvelope } from 'react-icons/fa';
+import { MdLockOutline, MdOutlineDriveFileRenameOutline } from 'react-icons/md';
 
 export default function RegisterForm() {
 	const [name, setName] = useState('');
@@ -61,37 +63,56 @@ export default function RegisterForm() {
 	};
 
 	return (
-		<div className='grid place-items-center h-screen'>
-			<div className='shadow-lg p-5 rounded-lg border-solid border-2 border-fuchsia-400 '>
-				<h1 className='text-2xl font-bold my-4'>Login Form</h1>
+		<div className='flex flex-col items-center justify-center min-h-screen bg-gray-100'>
+			<div className='shadow-2xl p-5 rounded-lg bg-white '>
+				<div className='text-left font-bold text-sm text-gray-700'>
+					<span className='text-fuchsia-500'>XYZ </span>Company
+				</div>
+				<h1 className='text-2xl font-bold mt-10 mb-8 text-center font-mono'>
+					Registration Form
+				</h1>
 
 				<form onSubmit={onSubmit} className='flex flex-col gap-6' action=''>
-					<input
-						onChange={(e) => setName(e.target.value)}
-						type='text'
-						placeholder='Full Name'
-					/>
-					<input
-						onChange={(e) => setEmail(e.target.value)}
-						type='email'
-						placeholder='email'
-					/>
-					<input
-						onChange={(e) => setPassword(e.target.value)}
-						type='password'
-						placeholder='password'
-					/>
-					<button className='bg-fuchsia-600 text-white font-bold cursor-pointer rounded-lg px-6 py-3 '>
-						REGISTER
-					</button>
+					<div className='bg-gray-100 w-70 p-2 flex items-center rounded-md pl-4 shadow-sm'>
+						<MdOutlineDriveFileRenameOutline className='text-gray-400 m-2' />
+						<input
+							onChange={(e) => setName(e.target.value)}
+							type='text'
+							placeholder='Full Name'
+							className='bg-gray-100 outline-none flex-1'
+						/>
+					</div>
+					<div className='bg-gray-100 w-70 p-2 flex items-center rounded-md pl-4 shadow-sm'>
+						<FaRegEnvelope className='text-gray-400 m-2' />
+						<input
+							onChange={(e) => setEmail(e.target.value)}
+							type='email'
+							placeholder='E-mail'
+							className='bg-gray-100 outline-none flex-1'
+						/>
+					</div>
+					<div className='bg-gray-100 w-70 p-2 flex items-center rounded-md pl-4 shadow-sm'>
+						<MdLockOutline className='text-gray-400 m-2' />
+						<input
+							onChange={(e) => setPassword(e.target.value)}
+							type='password'
+							placeholder='Password'
+							className='bg-gray-100 outline-none flex-1'
+						/>
+					</div>
+					<div className='mx-auto'>
+						<button className='bg-fuchsia-600 text-white font-bold cursor-pointer rounded-lg px-6 py-3 border-2 border-white hover:bg-white hover:text-fuchsia-600 hover:border-2 hover:border-solid hover:border-fuchsia-600'>
+							REGISTER
+						</button>
+					</div>
 					{error && (
 						<div className='bg-red-500 text-white w-fit text-sm py-1 px-3 rounded-sm mt-2'>
 							{error}
 						</div>
 					)}
-					<Link className='mt-3 text-right text-sm' href={'/'}>
-						Already Have an Account
-						<span className='underline'> Login</span>
+					<Link className='mt-3 text-right text-xs text-gray-600' href={'/'}>
+						Already Have an Account &nbsp;
+						<span className='underline'>Login</span>
 					</Link>
 				</form>
 			</div>
